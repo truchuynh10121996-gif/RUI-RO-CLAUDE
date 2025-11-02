5722145f2bdbddf049dc81cedda46ddf210f2578
truchuynh10121996-gif <truchuynh10121996@gmail.com>
2025-11-02 10:14:55 +0700

    Update ED.py

--- ED.py
+++ ED.py
@@ -659,7 +659,7 @@ if uploaded_file is not None:
 # THAY ĐỔI 4: Vị trí Tabs được giữ nguyên, CSS mới sẽ đảm bảo Tabs có màu
 # ------------------------------------------------------------------------------------------------
 tab_predict, tab_build, tab_goal = st.tabs([
-    "🚀 Sử dụng mô hình để dự báo", 
+    "🚀 TẢI HỒ SƠ ĐỂ DỰ BÁO", 
     "🛠️ Xây dựng mô hình", 
     "🎯 Mục tiêu của mô hình"
 ])
